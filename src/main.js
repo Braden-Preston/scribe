4403831e8be7ee04d2e0a053a69c4e74adce7622
Braden Preston <bpfilmsinc@gmail.com>
2021-09-17 16:04:55 -0500

    Basic templating example with htm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-// import './main.css'
+import htm from 'htm'
+import h from 'vhtml'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const html = htm.bind(h)
+
+const Header = ({ name }) => html`<h1>${name} List</h1>`
+
+const Footer = props => html`<footer ...${props} />`
+
+
+const App = () => {
+  const todos = ['cat', 'dog', 'steve']
+
+  return html`
+    <div>
+      <h1>Hello Vite!</h1>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">
+        Documentation
+      </a>
+      <${Header} name="My Todo" />
+      <ul>
+        ${todos.map(todo => html` <li key=${todo}>${todo}</li> `)}
+      </ul>
+      <button onClick=${() => this.addTodo()}>Add Todo</button>
+      <${Footer}>footer content here<//>
+    </div>
+  `
+}
+
+document.querySelector('#app').innerHTML = html`<${App} />`
