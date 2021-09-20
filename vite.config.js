c8aa9b9739dd3cdbbfd64dad80d48a05235f945e
Braden Preston <bpfilmsinc@gmail.com>
2021-09-19 20:34:11 -0500

    Add visualizer and compression

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
+import compression from 'vite-plugin-compression'
+import visualizer from 'rollup-plugin-visualizer'
+
+let analyze = process.argv.includes('--analyze')
 
 export default defineConfig({
-  root: './src',
+  root: 'src',
   build: {
     outDir: '../build',
     emptyOutDir: true
-  }
+  },
+  plugins: [
+    // Compress assets
+    compression({
+      threshold: 256,
+      verbose: false,
+      algorithm: 'brotliCompress',
+      ext: 'br'
+    }),
+    // Open a bundle visualizer
+    analyze &&
+      visualizer({
+        filename: 'build/report.html',
+        brotliSize: true,
+        open: true
+      })
+  ]
 })
