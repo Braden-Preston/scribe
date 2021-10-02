68b4aa3bea8fbeab58ec7b92b47bb9622abe5fda
Braden Preston <bpfilmsinc@gmail.com>
2021-10-02 14:20:53 -0500

    cleanup vite build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import compression from 'vite-plugin-compression'
 import visualizer from 'rollup-plugin-visualizer'
-import path from 'path'
 
 export default defineConfig(({ command, mode }) => {
   console.log(mode, command)
@@ -11,15 +10,13 @@ export default defineConfig(({ command, mode }) => {
     cacheDir: '../.yarn/cache/.vite',
     plugins: [
       // Compress assets
-      // compression({
-      //   ext: 'br',
-      //   algorithm: 'brotliCompress',
-      //   threshold: 256,
-      //   verbose: false,
-      //   filter: file => {
-      //     return /\.json/.exec(file) ? false : true
-      //   }
-      // })
+      compression({
+        ext: 'br',
+        algorithm: 'brotliCompress',
+        threshold: 256,
+        verbose: false,
+        filter: /\.(js|css|svg)$/i
+      })
     ],
     server: {
       open: process.argv.includes('--open')
@@ -31,30 +28,18 @@ export default defineConfig(({ command, mode }) => {
       outDir: '../build',
       manifest: true,
       emptyOutDir: true,
-      // cssCodeSplit: false,
-      // minify: false,
+      cssCodeSplit: false,
       rollupOptions: {
         input: {
-          app: path.resolve(__dirname, './src/index.html')
+          app: './src/index.html'
         },
-        external: ['quill-convert'],
         output: {
-          // Assign modules to chunk names
-          manualChunks: id => 'app',
           // Custom output naming
-          // chunkFileNames:  'asse/[name].js',
-          // entryFileNames:  (chunkInfo) => {
-          //   console.log(chunkInfo)
-          //   return 'cats/[name]-[hash].js'
-          // },
-          // chunkFileNames: ({ name }) =>
-          // name.endsWith('.br') ? 'assets/[name].[ext]' : '[name].js',
-          // entryFileNames: (chunkInfo) => {
-          //   console.log(chunkInfo)
-          //   return 'assets/[name].[ext]'
-          //   // return  name.endsWith('.br') ? 'assets/[name].[ext]' : '[name].js'
-          // },
-          // assetFileNames: 'assets/[name].[ext]'
+          assetFileNames: 'web/[name].[ext]',
+          entryFileNames: 'web/[name].js',
+          chunkFileNames: 'web/[name].js',
+          // Put all modules in single chunk
+          manualChunks: () => 'app',
         }
       }
     }
